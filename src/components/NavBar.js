203045657108ac8416f69304a9d4367af176fd18
+ <+>
2023-07-28 18:35:37 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const navLinkStyle = {
+  padding: "5px 10px",
+  color: "black",
+};
+
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" style={navLinkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={navLinkStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={navLinkStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={navLinkStyle}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
